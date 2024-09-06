6949e61c3ae50130bdcded98dd5be079ab3bd2d7
zhangzhixiong <751069244@qq.com>
2024-09-06 15:26:48 +0800

    refactor: 头发分割重构错误处理机制，支持图片模式

--- mediapipe/examples/desktop/hair_segmentation_sdk/hair_segmentation_sdk.cpp
+++ mediapipe/examples/desktop/hair_segmentation_sdk/hair_segmentation_sdk.cpp
@@ -7,50 +7,49 @@
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "hair_segmentation.h"
-#include "mediapipe/framework/calculator_framework.h"
-#include "mediapipe/framework/formats/image_frame.h"
-#include "mediapipe/framework/formats/image_frame_opencv.h"
-#include "mediapipe/framework/formats/landmark.pb.h"
-#include "mediapipe/framework/port/file_helpers.h"
-#include "mediapipe/framework/port/opencv_highgui_inc.h"
-#include "mediapipe/framework/port/opencv_imgproc_inc.h"
-#include "mediapipe/framework/port/opencv_video_inc.h"
-#include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status.h"
 
-MMPGraph *graph = nullptr;
+static MMPGraph *graph = nullptr;
 
-void initHairSeg() {
-  initHairSeg({});
-}
+static std::string last_error_message;
 
-void initHairSeg(std::vector<std::string> model_paths) {
+std::string getHairSegErrorMessages() { return last_error_message; }
+
+int initHairSeg() { return initHairSeg({}); }
+
+int initHairSeg(std::vector<std::string> model_paths) {
   google::InitGoogleLogging("HairSegSDK");
   graph = new MMPGraph();
-   absl::Status run_status;
+  absl::Status run_status;
   if (model_paths.size() == 0) {
     run_status = graph->InitMPPGraph();
   } else {
     run_status = graph->InitMPPGraph(model_paths);
   }
   if (!run_status.ok()) {
-    throw std::runtime_error(run_status.ToString());
+    last_error_message = run_status.ToString();
+    return run_status.raw_code();
   }
+  return 0;
 }
 
-void releaseHairSeg() {
+int releaseHairSeg() {
   google::ShutdownGoogleLogging();
   absl::Status run_status = graph->ReleaseMPPGraph();
-  if (!run_status.ok()) {
-    throw std::runtime_error(run_status.ToString());
-  }
   delete graph;
   graph = nullptr;
+  if (!run_status.ok()) {
+    last_error_message = run_status.ToString();
+    return run_status.raw_code();
+  }
+  return 0;
 }
 
-void getHairMask(const cv::Mat &img, cv::Mat &mask) {
+int getHairMask(const cv::Mat &img, cv::Mat &mask) {
   absl::Status run_status = graph->RunMPPGraph(img, mask);
   if (!run_status.ok()) {
-    throw std::runtime_error(run_status.ToString());
+    last_error_message = run_status.ToString();
+    return run_status.raw_code();
   }
+  return 0;
 }
